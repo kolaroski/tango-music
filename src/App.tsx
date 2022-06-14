3d22c724719c06cd6bcae320532c0a3de9fceb19
anarhitetka <a.urumovska@gmail.com>
2022-06-14 12:32:11 +0200

    add footer component in app

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import "./App.css";
 import { NavBar } from "./components/NavBar";
 import { CardList } from "./containers/CardList";
 import SideBar from "./containers/SideBar";
+import Footer from "./components/Footer";
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
         <h3 className="cardlistHeading">Orchestras</h3>
         <CardList titles={["001", "002", "003", "004", "005", "006"]} />
       </div>
+      <Footer />
     </div>
   );
 }
