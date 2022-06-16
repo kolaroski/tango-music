8412de7aaaaaa541fa87d2d859c0d2888235f25f
anarhitetka <a.urumovska@gmail.com>
2022-06-16 12:46:00 +0200

    merged backend and frontend code in main app file

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,18 +3,6 @@ import { NavBar } from "./components/NavBar";
 import { CardList } from "./containers/CardList";
 import SideBar from "./containers/SideBar";
 import Footer from "./components/Footer";
-
-function App() {
-  return (
-    <div>
-      <NavBar />
-      <SideBar />
-      <div>
-        <h3 className="cardlistHeading">Orchestras</h3>
-        <CardList titles={["001", "002", "003", "004", "005", "006"]} />
-      </div>
-      <Footer />
-import { CardList } from "./containers/CardList";
 import axios from "../node_modules/axios/index";
 import { useState } from "react";
 
@@ -34,36 +22,36 @@ function getAllSingers(): Promise<Array<string>> {
 }
 
 function App() {
-  const [all_orquestras, setAllOrquestras]= useState<Array<string>>([]);
-  getAllOrquestras()
-    .then(function (orquestras: Array<string>) {
-      setAllOrquestras(orquestras)
-    });
-  
-  const [all_singers, setAllSingers]= useState<Array<string>>([]);
-  // Fetch all the orquestras and all the singers
-  getAllOrquestras()
-    .then(function (orquestras: Array<string>) {
-      setAllOrquestras(orquestras)
-    });
+  const [all_orquestras, setAllOrquestras] = useState<Array<string>>([]);
+  getAllOrquestras().then(function (orquestras: Array<string>) {
+    setAllOrquestras(orquestras);
+  });
 
-  getAllSingers()
-  .then(function (singers: Array<string>) {
-    setAllSingers(singers)
+  const [all_singers, setAllSingers] = useState<Array<string>>([]);
+  // Fetch all the orquestras and all the singers
+  getAllOrquestras().then(function (orquestras: Array<string>) {
+    setAllOrquestras(orquestras);
+  });
+
+  getAllSingers().then(function (singers: Array<string>) {
+    setAllSingers(singers);
   });
 
   return (
     <div>
-      <div>
-        <h1 className="testColor">Go to hell(o) world.</h1>
-        <h2>tango-music-project</h2>
+      <NavBar />
+      <SideBar />
+      <div className="main-content">
+        <div>
+          <h3 className="cardlistHeading">Orchestras</h3>
+          <CardList titles={all_orquestras} />
+        </div>
+        <div>
+          <h3 className="cardlistHeading">Singers</h3>
+          <CardList titles={all_singers} />
+        </div>
       </div>
-      <div className="displayStyle">
-        <CardList titles={all_orquestras} />
-      </div>
-      <div className="displayStyle">
-        <CardList titles={all_singers} />
-      </div>
+      <Footer />
     </div>
   );
 }
