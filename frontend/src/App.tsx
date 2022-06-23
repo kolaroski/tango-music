73841557d44b4523a45426781f19e777930f50c2
Kire Kolaroski <kolaroski.kire@outlook.com>
2022-06-23 18:48:58 +0200

    Improve the API calls

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -4,7 +4,7 @@ import { CardList } from "./containers/CardList";
 import SideBar from "./containers/SideBar";
 import Footer from "./components/Footer";
 import axios from "../node_modules/axios/index";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 const ORQUESTRAS_URL = "http://localhost:8000/all/orquestras";
 const SINGERS_URL = "http://localhost:8000/all/singers";
@@ -23,19 +23,17 @@ function getAllSingers(): Promise<Array<string>> {
 
 function App() {
   const [all_orquestras, setAllOrquestras] = useState<Array<string>>([]);
-  getAllOrquestras().then(function (orquestras: Array<string>) {
-    setAllOrquestras(orquestras);
-  });
-
   const [all_singers, setAllSingers] = useState<Array<string>>([]);
   // Fetch all the orquestras and all the singers
-  getAllOrquestras().then(function (orquestras: Array<string>) {
-    setAllOrquestras(orquestras);
-  });
+  useEffect(() => {
+    getAllOrquestras().then(function (orquestras: Array<string>) {
+      setAllOrquestras(orquestras);
+    });
 
-  getAllSingers().then(function (singers: Array<string>) {
-    setAllSingers(singers);
-  });
+    getAllSingers().then(function (singers: Array<string>) {
+      setAllSingers(singers);
+    });
+  }, [])
 
   return (
     <div>
